ae719b85c826d539b3e5c09e6bcd442b426ca63c
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2016-06-02 15:12:57 +0200

    debug: add another osx FLAGS

--- recipe/build.sh
+++ recipe/build.sh
@@ -16,11 +16,11 @@ then
     # for Mac OSX
     export CC=clang
     export CXX=clang++
-#     export MACOSX_VERSION_MIN="10.7"
-#     export MACOSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}"
-#     export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+    export MACOSX_VERSION_MIN="10.7"
+    export MACOSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}"
+    export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
     export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
-#     export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+    export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
     export LDFLAGS="${LDFLAGS} -stdlib=libc++ -lc++"
     export LINKFLAGS="${LDFLAGS}"
 #     # See http://www.unidata.ucar.edu/support/help/MailArchives/netcdf/msg11939.html
